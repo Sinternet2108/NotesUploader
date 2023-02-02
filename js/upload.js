2ff3003d3b3ca60b3fce711934a2b54d3d3d1c1e
Rakshitha Patel <rakshithapatel0807@gmail.com>
2023-02-02 15:07:22 +0530

    formImproved

--- js/upload.js
+++ js/upload.js
@@ -5,19 +5,24 @@ const uploadButton = document.getElementById("upload-button");
 const divButton = document.getElementById("div-button");
 const formElement = document.getElementById("upload-form");
 const uploadForm = document.getElementById("form");
+const loadElement = document.querySelector(".load");
 
 inputElement.addEventListener("change",(event)=>{
 
     inputElement.addEventListener("click",()=>{
+        loadElement.style.setProperty("display","block");
         tickElement.style.setProperty("transform","scale(0)");
+        tickElement.style.setProperty("transition","transform 0s");
     })
 
     tickElement.style.setProperty("transform","scale(1)");
+    tickElement.style.setProperty("transition","transform 1s ease");
     divButton.style.setProperty("display","block");
     fileElement.style.setProperty("display","none");
+    loadElement.style.setProperty("display","none");
 });
 
 divButton.addEventListener("click",()=>{
     // uploadForm.style.setProperty("filter","blur(30px)");
-    formElement.style.setProperty("display","block");
+    formElement.style.setProperty("height","90vh");
 });
